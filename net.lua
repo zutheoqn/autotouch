f67b6d7e012992a04fa97b2e7a11734da355fe78
zutheoqn <33539894+zutheoqn@users.noreply.github.com>
2022-02-21 06:41:20 +0700

    Update net.lua

--- net.lua
+++ net.lua
@@ -1,4 +1,4 @@
---ver 1.3
+--ver 1.31
 --soucre https://github.com/zutheoqn/autotouch.git
 local xoainfo = "com.ienthach.XoaInfo" ;
 local key_note = "com.apple.mobilenotes" ;
@@ -10,9 +10,9 @@ local offwall_loc_x,offwall_loc_y,offwall_loc_color = 694, 465, 7961213;
 --com.app.armorvpn, 203,1205,11267771 
 --irlapp.com.IRL  , 332,1140,14282239
 local app_1,app_1_loc_x,app_1_loc_y,app_1_loc_rx,app_1_loc_ry = "com.app.armorvpn",203,1205,math.random(100,200),math.random(500,600);
---local app_2,app_2_loc_x,app_2_loc_y,app_2_loc_rx,app_2_loc_ry = "co.mona.Monaco",332,1140,math.random(100,200),math.random(500,600);
+local app_2,app_2_loc_x,app_2_loc_y,app_2_loc_rx,app_2_loc_ry = "co.mona.Monaco",332,1140,math.random(100,200),math.random(500,600);
 local app_3,app_3_loc_x,app_3_loc_y,app_3_loc_rx,app_3_loc_ry = "irlapp.com.IRL",332,1140,math.random(100,200),math.random(500,600);
---local app_4,app_4_loc_x,app_4_loc_y,app_4_loc_rx,app_4_loc_ry = "connectiq.miles",332,1140,math.random(100,200),math.random(500,600);
+local app_4,app_4_loc_x,app_4_loc_y,app_4_loc_rx,app_4_loc_ry = "connectiq.miles",332,1140,math.random(100,200),math.random(500,600);
 
 function ssleep(s)
       sc = s * 1000000 ;
@@ -35,7 +35,7 @@ function _clearDB()--open,clearDB xoainfo
                   toast('_clearDB', 1);
                   ssleep(1);
             end
-      until appState(xoainfo) ~= "ACTIVATED" or appState(key_note) == "ACTIVATED"
+      until appState(xoainfo) ~= "ACTIVATED"
 end
 function _openURL(url)
       openURL(url);
@@ -67,9 +67,6 @@ function _sleepApp(app,x,y,rx,ry)
                   ssleep(10);
                   tap(rx,ry);
                   ssleep(10);
-            elseif appState(key_note) == "ACTIVATED" then
-                  ssleep(1);
-                  break;
             else
                   ssleep(1);
                   break;
@@ -78,7 +75,7 @@ function _sleepApp(app,x,y,rx,ry)
       --random click
 end
 
-repeat
+
       ssleep(2);
       _clearDB();
       ssleep(2);
@@ -92,11 +89,11 @@ repeat
             if appState(app_key_open) == "ACTIVATED" then
                   ssleep(2);
                   _sleepApp(app_1,app_1_loc_x,app_1_loc_y,app_1_loc_rx,app_1_loc_ry);
-                  --_sleepApp(app_2,app_2_loc_x,app_2_loc_y);
+                  _sleepApp(app_2,app_2_loc_x,app_2_loc_y,app_2_loc_rx,app_2_loc_ry);
                   _sleepApp(app_3,app_3_loc_x,app_3_loc_y,app_3_loc_rx,app_3_loc_ry);
-                  --_sleepApp(app_4,app_4_loc_x,app_4_loc_y);
+                  _sleepApp(app_4,app_4_loc_x,app_4_loc_y,app_4_loc_rx,app_4_loc_ry);
                   stop_loop_openApp = true;
             end
       until stop_loop_openApp or appState(key_note) == "ACTIVATED"
-until appState(key_note) == "ACTIVATED"
+
 
